fa0a47ea29b9ef3746f924032429c3decb9af9f0
Naveen V Patil <naveenpatil26@gmail.com>
2017-04-07 18:54:02 +0530

    Update SerialVersion.py

--- Python Parallel/SerialVersion.py
+++ Python Parallel/SerialVersion.py
@@ -1,8 +1,13 @@
+#This code achieves similar functionality as the parallel code.
+
 import time
 import random
 import math
 from multiprocessing import Pool
 
+# This function tries to find an element in the list hose cube root is equal to 102..
+# This is litrally impossible, since we are generating numbers between 1 and 102
+# This is done intentionally so that the entire list is being iteated through.
 
 def CostlyFunction(x):
 	for i in x:
