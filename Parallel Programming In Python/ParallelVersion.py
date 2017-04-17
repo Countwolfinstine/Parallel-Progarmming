386414745609e84caa41040e0e568b4f7ec19e6b
Countwolfinstine <naveenpatil26@gmail.com>
2017-04-17 19:22:39 +0530

    redme

--- Parallel Programming In Python/ParallelVersion.py
+++ Parallel Programming In Python/ParallelVersion.py
@@ -1,37 +1,8 @@
-# Sample Python MultiProcessing Code to demonstrate the use of Process method of multiprocessing library.
-# The code iterates over two randomly generated lists at the same time. 
-# Each list element is also raised to the power 1/3.0 and added to a new list which is returned int the end 
-# This Code is similar to writing Parallel OMP setions directive of Open MP in C programming. 
-
-import time
-import random
-from multiprocessing import Pool
-from multiprocessing import Process , Queue
-
-def CostlyFunction(x,q1):
-	arr=[]
-	for i in x:
-		 arr.append(i**(1 / 3.0))
-	q1.put(arr)
-
-def GenerateRandomList():
-	arr=[]
-	for y in range(1,1000000):
-		arr.append(random.randint(1,101))
-	return arr
-
-if __name__=='__main__':
-	q1=Queue()
-	t0=time.clock()
-	List1=GenerateRandomList()
-	List2=GenerateRandomList()
-	p = Process(target=CostlyFunction,args=(List1,q1,))
-	q = Process(target=CostlyFunction,args=(List2,q1,))
-	q.start()
-	p.start()
-	List1=q1.get()
-	List2=q1.get()
-	q.join()
-	p.join()
-	t1=time.clock()
-	print(t1-t0)
+<pre><h2>How to Run the code:</h2>
+- Use the following command to run the parallel version.
+	<code>python ParallelVersion.py > Output/a.txt </code>	
+- Use the following command to run the serial version.
+	<code>python SerialVersion.py > Output/b.txt </code>
+- a.txt file will be created when the first command is executed and b.txt is created when the second command is executed. The number in the file indicates the time taken by the program to complete the execution.
+- You can clearly observe that the time taken by parallel version of the code is lesser than the serial version
+</pre>
